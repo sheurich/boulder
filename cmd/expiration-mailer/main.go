sheurich/boulder

commit a1bb8654bca29fef6a2a23982881a6ac877c7249

Generate and store OCSP at precertificate signing time (#4420)
This change adds two tables and two methods in the SA, to store precertificates
and serial numbers.

In the CA, when the feature flag is turned on, we generate a serial number, store it,
sign a precertificate and OCSP, store them, and then return the precertificate. Storing
the serial as an additional step before signing the certificate adds an extra layer of
insurance against duplicate serials, and also serves as a check on database availability.
Since an error storing the serial prevents going on to sign the precertificate, this decreases
the chance of signing something while the database is down.

Right now, neither table has read operations available in the SA.

To make this work, I needed to remove the check for duplicate certificateStatus entry
when inserting a final certificate and its OCSP response. I also needed to remove
an error that can occur when expiration-mailer processes a precertificate that lacks
a final certificate. That error would otherwise have prevented further processing of
expiration warnings.

Fixes #4412

This change builds on #4417, please review that first for ease of review.

cmd/expiration-mailer/main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"crypto/x509"
+	"database/sql"
 	"errors"
 	"flag"
 	"fmt"
@@ -302,6 +303,13 @@ func (m *mailer) findExpiringCertificates() error {
 			var cert core.Certificate
 			cert, err := sa.SelectCertificate(m.dbMap, "WHERE serial = ?", serial)
 			if err != nil {
+				// We can get an ErrNoRows when processing a serial number corresponding
+				// to a precertificate with no final certificate. Since this certificate
+				// is not being used by a subscriber, we don't send expiration email about
+				// it.
+				if err == sql.ErrNoRows {
+					continue
+				}
 				m.log.AuditErrf("expiration-mailer: Error loading cert %q: %s", cert.Serial, err)
 				return err
 			}

ca/ca.go
@@ -27,6 +27,9 @@ import (
 	"github.com/google/certificate-transparency-go"
 	cttls "github.com/google/certificate-transparency-go/tls"
 	"github.com/jmhodges/clock"
+	corepb "github.com/letsencrypt/boulder/core/proto"
+	"github.com/letsencrypt/boulder/features"
+	sapb "github.com/letsencrypt/boulder/sa/proto"
 	"github.com/miekg/pkcs11"
 
 	"github.com/letsencrypt/boulder/ca/config"
@@ -99,6 +102,8 @@ const (
 
 type certificateStorage interface {
 	AddCertificate(context.Context, []byte, int64, []byte, *time.Time) (string, error)
+	AddPrecertificate(ctx context.Context, req *sapb.AddCertificateRequest) (*corepb.Empty, error)
+	AddSerial(ctx context.Context, req *sapb.AddSerialRequest) (*corepb.Empty, error)
 }
 
 type certificateType string
@@ -427,13 +432,61 @@ func (ca *CertificateAuthorityImpl) IssuePrecertificate(ctx context.Context, iss
 		return nil, err
 	}
 
-	precertDER, err := ca.issuePrecertificateInner(ctx, issueReq, serialBigInt, validity, precertType)
-	if err != nil {
-		return nil, err
+	if features.Enabled(features.PrecertificateOCSP) {
+		serialHex := core.SerialToString(serialBigInt)
+		nowNanos := ca.clk.Now().UnixNano()
+		expiresNanos := validity.NotAfter.UnixNano()
+		_, err = ca.sa.AddSerial(ctx, &sapb.AddSerialRequest{
+			Serial:  &serialHex,
+			RegID:   issueReq.RegistrationID,
+			Created: &nowNanos,
+			Expires: &expiresNanos,
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		precertDER, err := ca.issuePrecertificateInner(ctx, issueReq, serialBigInt, validity, precertType)
+		if err != nil {
+			return nil, err
+		}
+
+		ocspResp, err := ca.GenerateOCSP(ctx, core.OCSPSigningRequest{
+			CertDER: precertDER,
+			Status:  string(core.OCSPStatusGood),
+		})
+		if err != nil {
+			err = berrors.InternalServerError(err.Error())
+			ca.log.AuditInfof("OCSP Signing failure: serial=[%s] err=[%s]", serialHex, err)
+		}
+
+		_, err = ca.sa.AddPrecertificate(ctx, &sapb.AddCertificateRequest{
+			Der:    precertDER,
+			RegID:  issueReq.RegistrationID,
+			Ocsp:   ocspResp,
+			Issued: &nowNanos,
+		})
+		if err != nil {
+			err = berrors.InternalServerError(err.Error())
+			// TODO(#4425): Extend orphanQueue support to precertificates.
+			ca.log.AuditErrf("Failed RPC to store at SA, orphaning precertificate: serial=[%s] cert=[%s] err=[%v], regID=[%d], orderID=[%d]",
+				serialHex, hex.EncodeToString(precertDER), err, issueReq.RegistrationID, issueReq.OrderID)
+			return nil, err
+		}
+
+		return &caPB.IssuePrecertificateResponse{
+			DER: precertDER,
+		}, nil
+
+	} else {
+		precertDER, err := ca.issuePrecertificateInner(ctx, issueReq, serialBigInt, validity, precertType)
+		if err != nil {
+			return nil, err
+		}
+		return &caPB.IssuePrecertificateResponse{
+			DER: precertDER,
+		}, nil
 	}
-	return &caPB.IssuePrecertificateResponse{
-		DER: precertDER,
-	}, nil
 }
 
 // IssueCertificateForPrecertificate takes a precertificate and a set of SCTs for that precertificate
@@ -626,16 +679,20 @@ func (ca *CertificateAuthorityImpl) generateOCSPAndStoreCertificate(
 	orderID int64,
 	serialBigInt *big.Int,
 	certDER []byte) (core.Certificate, error) {
-	ocspResp, err := ca.GenerateOCSP(ctx, core.OCSPSigningRequest{
-		CertDER: certDER,
-		Status:  "good",
-	})
-	if err != nil {
-		err = berrors.InternalServerError(err.Error())
-		ca.log.AuditInfof("OCSP Signing failure: serial=[%s] err=[%s]", core.SerialToString(serialBigInt), err)
-		// Ignore errors here to avoid orphaning the certificate. The
-		// ocsp-updater will look for certs with a zero ocspLastUpdated
-		// and generate the initial response in this case.
+	var err error
+	var ocspResp []byte
+	if !features.Enabled(features.PrecertificateOCSP) {
+		ocspResp, err = ca.GenerateOCSP(ctx, core.OCSPSigningRequest{
+			CertDER: certDER,
+			Status:  string(core.OCSPStatusGood),
+		})
+		if err != nil {
+			err = berrors.InternalServerError(err.Error())
+			ca.log.AuditInfof("OCSP Signing failure: serial=[%s] err=[%s]", core.SerialToString(serialBigInt), err)
+			// Ignore errors here to avoid orphaning the certificate. The
+			// ocsp-updater will look for certs with a zero ocspLastUpdated
+			// and generate the initial response in this case.
+		}
 	}
 
 	now := ca.clk.Now()

ca/ca_test.go
@@ -34,11 +34,13 @@ import (
 	caPB "github.com/letsencrypt/boulder/ca/proto"
 	"github.com/letsencrypt/boulder/cmd"
 	"github.com/letsencrypt/boulder/core"
+	corepb "github.com/letsencrypt/boulder/core/proto"
 	berrors "github.com/letsencrypt/boulder/errors"
 	"github.com/letsencrypt/boulder/goodkey"
 	blog "github.com/letsencrypt/boulder/log"
 	"github.com/letsencrypt/boulder/metrics"
 	"github.com/letsencrypt/boulder/policy"
+	sapb "github.com/letsencrypt/boulder/sa/proto"
 	"github.com/letsencrypt/boulder/test"
 )
 
@@ -173,6 +175,14 @@ type mockSA struct {
 func (m *mockSA) AddCertificate(ctx context.Context, der []byte, _ int64, _ []byte, _ *time.Time) (string, error) {
 	m.certificate.DER = der
 	return "", nil
+}
+
+func (m *mockSA) AddPrecertificate(ctx context.Context, req *sapb.AddCertificateRequest) (*corepb.Empty, error) {
+	return &corepb.Empty{}, nil
+}
+
+func (m *mockSA) AddSerial(ctx context.Context, req *sapb.AddSerialRequest) (*corepb.Empty, error) {
+	return &corepb.Empty{}, nil
 }
 
 var caKey crypto.Signer
@@ -899,6 +909,14 @@ func (qsa *queueSA) AddCertificate(_ context.Context, _ []byte, _ int64, _ []byt
 	}
 	qsa.issued = issued
 	return "", nil
+}
+
+func (qsa *queueSA) AddPrecertificate(ctx context.Context, req *sapb.AddCertificateRequest) (*corepb.Empty, error) {
+	return &corepb.Empty{}, nil
+}
+
+func (qsa *queueSA) AddSerial(ctx context.Context, req *sapb.AddSerialRequest) (*corepb.Empty, error) {
+	return &corepb.Empty{}, nil
 }
 
 func TestOrphanQueue(t *testing.T) {

core/interfaces.go
@@ -149,6 +149,8 @@ type StorageAdder interface {
 	NewPendingAuthorization(ctx context.Context, authz Authorization) (Authorization, error)
 	FinalizeAuthorization(ctx context.Context, authz Authorization) error
 	AddCertificate(ctx context.Context, der []byte, regID int64, ocsp []byte, issued *time.Time) (digest string, err error)
+	AddPrecertificate(ctx context.Context, req *sapb.AddCertificateRequest) (*corepb.Empty, error)
+	AddSerial(ctx context.Context, req *sapb.AddSerialRequest) (*corepb.Empty, error)
 	DeactivateRegistration(ctx context.Context, id int64) error
 	DeactivateAuthorization(ctx context.Context, id string) error
 	NewOrder(ctx context.Context, order *corepb.Order) (*corepb.Order, error)

features/featureflag_string.go
@@ -34,11 +34,12 @@ func _() {
 	_ = x[ParallelCheckFailedValidation-23]
 	_ = x[DeleteUnusedChallenges-24]
 	_ = x[V1DisableNewValidations-25]
+	_ = x[PrecertificateOCSP-26]
 }
 
-const _FeatureFlag_name = "unusedPerformValidationRPCACME13KeyRolloverSimplifiedVAHTTPTLSSNIRevalidationAllowRenewalFirstRLSetIssuedNamesRenewalBitFasterRateLimitProbeCTLogsRevokeAtRACAAValidationMethodsCAAAccountURIHeadNonceStatusOKNewAuthorizationSchemaDisableAuthz2OrdersEarlyOrderRateLimitEnforceMultiVAMultiVAFullResultsRemoveWFE2AccountIDCheckRenewalFirstMandatoryPOSTAsGETFasterGetOrderForNamesAllowV1RegistrationParallelCheckFailedValidationDeleteUnusedChallengesV1DisableNewValidations"
+const _FeatureFlag_name = "unusedPerformValidationRPCACME13KeyRolloverSimplifiedVAHTTPTLSSNIRevalidationAllowRenewalFirstRLSetIssuedNamesRenewalBitFasterRateLimitProbeCTLogsRevokeAtRACAAValidationMethodsCAAAccountURIHeadNonceStatusOKNewAuthorizationSchemaDisableAuthz2OrdersEarlyOrderRateLimitEnforceMultiVAMultiVAFullResultsRemoveWFE2AccountIDCheckRenewalFirstMandatoryPOSTAsGETFasterGetOrderForNamesAllowV1RegistrationParallelCheckFailedValidationDeleteUnusedChallengesV1DisableNewValidationsPrecertificateOCSP"
 
-var _FeatureFlag_index = [...]uint16{0, 6, 26, 43, 59, 77, 96, 120, 135, 146, 156, 176, 189, 206, 228, 247, 266, 280, 298, 317, 334, 352, 374, 393, 422, 444, 467}
+var _FeatureFlag_index = [...]uint16{0, 6, 26, 43, 59, 77, 96, 120, 135, 146, 156, 176, 189, 206, 228, 247, 266, 280, 298, 317, 334, 352, 374, 393, 422, 444, 467, 485}
 
 func (i FeatureFlag) String() string {
 	if i < 0 || i >= FeatureFlag(len(_FeatureFlag_index)-1) {

features/features.go
@@ -68,6 +68,10 @@ const (
 	// V1DisableNewValidations disables validations for new domain names in the V1
 	// API.
 	V1DisableNewValidations
+	// PrecertificateOCSP ensures that we write an OCSP response immediately upon
+	// generating a precertificate. This also changes the issuance / storage flow,
+	// adding two new calls from CA to SA: AddSerial and AddPrecertificate.
+	PrecertificateOCSP
 )
 
 // List of features and their default value, protected by fMu
@@ -98,6 +102,7 @@ var features = map[FeatureFlag]bool{
 	ParallelCheckFailedValidation: false,
 	DeleteUnusedChallenges:        false,
 	V1DisableNewValidations:       false,
+	PrecertificateOCSP:            false,
 }
 
 var fMu = new(sync.RWMutex)

grpc/sa-wrappers.go
@@ -283,6 +283,34 @@ func (sac StorageAuthorityClientWrapper) PreviousCertificateExists(
 	return exists, err
 }
 
+func (sac StorageAuthorityClientWrapper) AddPrecertificate(
+	ctx context.Context,
+	req *sapb.AddCertificateRequest,
+) (*corepb.Empty, error) {
+	empty, err := sac.inner.AddPrecertificate(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	if empty == nil {
+		return nil, errIncompleteResponse
+	}
+	return empty, nil
+}
+
+func (sac StorageAuthorityClientWrapper) AddSerial(
+	ctx context.Context,
+	req *sapb.AddSerialRequest,
+) (*corepb.Empty, error) {
+	empty, err := sac.inner.AddSerial(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	if empty == nil {
+		return nil, errIncompleteResponse
+	}
+	return empty, nil
+}
+
 func (sac StorageAuthorityClientWrapper) FQDNSetExists(ctx context.Context, domains []string) (bool, error) {
 	response, err := sac.inner.FQDNSetExists(ctx, &sapb.FQDNSetExistsRequest{Domains: domains})
 	if err != nil {
@@ -615,10 +643,11 @@ func (sas StorageAuthorityClientWrapper) DeactivateAuthorization2(ctx context.Co
 type StorageAuthorityServerWrapper struct {
 	// TODO(#3119): Don't use core.StorageAuthority
 	inner core.StorageAuthority
+	core.StorageAuthority
 }
 
 func NewStorageAuthorityServer(inner core.StorageAuthority) *StorageAuthorityServerWrapper {
-	return &StorageAuthorityServerWrapper{inner}
+	return &StorageAuthorityServerWrapper{inner, inner}
 }
 
 func (sas StorageAuthorityServerWrapper) GetRegistration(ctx context.Context, request *sapb.RegistrationID) (*corepb.Registration, error) {

mocks/mocks.go
@@ -319,6 +319,16 @@ func (sa *StorageAuthority) GetCertificateStatus(_ context.Context, serial strin
 	}
 }
 
+// AddPrecertificate is a mock
+func (sa *StorageAuthority) AddPrecertificate(ctx context.Context, req *sapb.AddCertificateRequest) (empty *corepb.Empty, err error) {
+	return
+}
+
+// AddSerial is a mock
+func (sa *StorageAuthority) AddSerial(ctx context.Context, req *sapb.AddSerialRequest) (empty *corepb.Empty, err error) {
+	return
+}
+
 // AddCertificate is a mock
 func (sa *StorageAuthority) AddCertificate(_ context.Context, certDER []byte, regID int64, _ []byte, _ *time.Time) (digest string, err error) {
 	return

commit 3f7f760a07cee6450508a024be5b03beb16c634c

Switch nag group to count serials, not certs (#5605)
If expiry mailer's SELECT statement is regularly returning exactly
as many rows as it is configured to limit at, that means that we are
consistently missing some certs that we should probably be sending
emails about. Change the way this metric is computed to be based
on the number of rows returned directly by the SELECT, rather than
based on the filtered set of rows that ended up corresponding to
final certificates.

This bug was almost certainly causing us to not detect nag groups
being at capacity, given that the expiry-mailer logs consistently say
that nag groups have nearly-but-not-quite as many certs in them as
the configured maximum. This bug was introduced in #4420, when
expiration-mailer gained the ability to silently continue past certain
classes of errors retrieving certificate details.

cmd/expiration-mailer/main.go
@@ -296,6 +296,21 @@ func (m *mailer) findExpiringCertificates() error {
 			return err
 		}
 
+		// If the number of rows was exactly `m.limit` rows we need to increment
+		// a stat indicating that this nag group is at capacity based on the
+		// configured cert limit. If this condition continually occurs across mailer
+		// runs then we will not catch up, resulting in under-sending expiration
+		// mails. The effects of this were initially described in issue #2002[0].
+		//
+		// 0: https://github.com/letsencrypt/boulder/issues/2002
+		atCapacity := float64(0)
+		if len(serials) == m.limit {
+			m.log.Infof("nag group %s expiring certificates at configured capacity (select limit %d)",
+				expiresIn.String(), m.limit)
+			atCapacity = float64(1)
+		}
+		m.stats.nagsAtCapacity.With(prometheus.Labels{"nag_group": expiresIn.String()}).Set(atCapacity)
+
 		// Now we can sequentially retrieve the certificate details for each of the
 		// certificate status rows
 		var certs []core.Certificate
@@ -318,21 +333,6 @@ func (m *mailer) findExpiringCertificates() error {
 
 		m.log.Infof("Found %d certificates expiring between %s and %s", len(certs),
 			left.Format("2006-01-02 03:04"), right.Format("2006-01-02 03:04"))
-
-		// If the `certs` result was exactly `m.limit` rows we need to increment
-		// a stat indicating that this nag group is at capacity based on the
-		// configured cert limit. If this condition continually occurs across mailer
-		// runs then we will not catch up, resulting in under-sending expiration
-		// mails. The effects of this were initially described in issue #2002[0].
-		//
-		// 0: https://github.com/letsencrypt/boulder/issues/2002
-		atCapacity := float64(0)
-		if len(certs) == m.limit {
-			m.log.Infof("nag group %s expiring certificates at configured capacity (cert limit %d)",
-				expiresIn.String(), m.limit)
-			atCapacity = float64(1)
-		}
-		m.stats.nagsAtCapacity.With(prometheus.Labels{"nag_group": expiresIn.String()}).Set(atCapacity)
 
 		if len(certs) == 0 {
 			continue // nothing to do