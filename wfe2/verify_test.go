sheurich/boulder

commit 292b16fb930e46d1c3a2c9dfcadac85109d791e2

WFE: Add custom balancer implementation which routes nonce redemption RPCs by prefix (#6618)
Assign nonce prefixes for each nonce-service by taking the first eight
characters of the the base64url encoded HMAC-SHA256 hash of the RPC
listening address using a provided key. The provided key must be same
across all boulder-wfe and nonce-service instances.
- Add a custom `grpc-go` load balancer implementation (`nonce`) which
can route nonce redemption RPC messages by matching the prefix to the
derived prefix of the nonce-service instance which created it.
- Modify the RPC client constructor to allow the operator to override
the default load balancer implementation (`round_robin`).
- Modify the `srv` RPC resolver to accept a comma separated list of
targets to be resolved.
- Remove unused nonce-service `-prefix` flag.

Fixes #6404

grpc/noncebalancer/noncebalancer.go
@@ -1,0 +1,112 @@
+package noncebalancer
+
+import (
+	"errors"
+
+	"github.com/letsencrypt/boulder/nonce"
+	"google.golang.org/grpc/balancer"
+	"google.golang.org/grpc/balancer/base"
+)
+
+const (
+	// Name is the name used to register the nonce balancer with the gRPC
+	// runtime.
+	Name = "nonce"
+
+	// SRVResolverScheme is the scheme used to invoke an instance of the SRV
+	// resolver which will use the noncebalancer to pick backends. It would be
+	// ideal to export this from the SRV resolver package but that package is
+	// internal.
+	SRVResolverScheme = "nonce-srv"
+)
+
+var errMissingPrefixCtxKey = errors.New("nonce.PrefixCtxKey value required in RPC context")
+var errMissingHMACKeyCtxKey = errors.New("nonce.HMACKeyCtxKey value required in RPC context")
+var errInvalidPrefixCtxKeyType = errors.New("nonce.PrefixCtxKey value in RPC context must be a string")
+var errInvalidHMACKeyCtxKeyType = errors.New("nonce.HMACKeyCtxKey value in RPC context must be a string")
+
+// Balancer implements the base.PickerBuilder interface. It's used to create new
+// balancer.Picker instances. It should only be used by nonce-service clients.
+type Balancer struct{}
+
+// Compile-time assertion that *Balancer implements the base.PickerBuilder
+// interface.
+var _ base.PickerBuilder = (*Balancer)(nil)
+
+// Build implements the base.PickerBuilder interface. It is called by the gRPC
+// runtime when the balancer is first initialized and when the set of backend
+// (SubConn) addresses changes. It is responsible for initializing the Picker's
+// backends map and returning a balancer.Picker.
+func (b *Balancer) Build(buildInfo base.PickerBuildInfo) balancer.Picker {
+	return &Picker{
+		backends: buildInfo.ReadySCs,
+	}
+}
+
+// Picker implements the balancer.Picker interface. It picks a backend (SubConn)
+// based on the nonce prefix contained in each request's Context.
+type Picker struct {
+	backends        map[balancer.SubConn]base.SubConnInfo
+	prefixToBackend map[string]balancer.SubConn
+}
+
+// Compile-time assertion that *Picker implements the balancer.Picker interface.
+var _ balancer.Picker = (*Picker)(nil)
+
+// Pick implements the balancer.Picker interface. It is called by the gRPC
+// runtime for each RPC message. It is responsible for picking a backend
+// (SubConn) based on the context of each RPC message.
+func (p *Picker) Pick(info balancer.PickInfo) (balancer.PickResult, error) {
+	if len(p.backends) == 0 {
+		// The Picker must be rebuilt if there are no backends available.
+		return balancer.PickResult{}, balancer.ErrNoSubConnAvailable
+	}
+
+	// Get the HMAC key from the RPC context.
+	hmacKeyVal := info.Ctx.Value(nonce.HMACKeyCtxKey{})
+	if hmacKeyVal == nil {
+		// This should never happen.
+		return balancer.PickResult{}, errMissingHMACKeyCtxKey
+	}
+	hmacKey, ok := hmacKeyVal.(string)
+	if !ok {
+		// This should never happen.
+		return balancer.PickResult{}, errInvalidHMACKeyCtxKeyType
+	}
+
+	if p.prefixToBackend == nil {
+		// Iterate over the backends and build a map of the derived prefix for
+		// each backend SubConn.
+		prefixToBackend := make(map[string]balancer.SubConn)
+		for sc, scInfo := range p.backends {
+			scPrefix := nonce.DerivePrefix(scInfo.Address.Addr, hmacKey)
+			prefixToBackend[scPrefix] = sc
+		}
+		p.prefixToBackend = prefixToBackend
+	}
+
+	// Get the destination prefix from the RPC context.
+	destPrefixVal := info.Ctx.Value(nonce.PrefixCtxKey{})
+	if destPrefixVal == nil {
+		// This should never happen.
+		return balancer.PickResult{}, errMissingPrefixCtxKey
+	}
+	destPrefix, ok := destPrefixVal.(string)
+	if !ok {
+		// This should never happen.
+		return balancer.PickResult{}, errInvalidPrefixCtxKeyType
+	}
+
+	sc, ok := p.prefixToBackend[destPrefix]
+	if !ok {
+		// No backend SubConn was found for the destination prefix.
+		return balancer.PickResult{}, balancer.ErrNoSubConnAvailable
+	}
+	return balancer.PickResult{SubConn: sc}, nil
+}
+
+func init() {
+	balancer.Register(
+		base.NewBalancerBuilder(Name, &Balancer{}, base.Config{}),
+	)
+}

commit 0dbb8d13b18f8cd69a5301438e335a5872e65b01

WFE: Correct Error Handling for Nonce Redemption RPCs with Unknown Prefixes (#7004)
Fix an issue related to the custom gRPC Picker implementation introduced
in #6618. When a nonce contained a prefix not associated with a known
backend, the Picker would continuously rebuild, re-resolve DNS, and
eventually throw a 500 "Server Error" at RPC timeout. The Picker now
promptly returns a 400 "Bad Nonce" error as expected, in response the
requesting client should retry their request with a fresh nonce.

Additionally:
- WFE unit tests use derived nonces when `"BOULDER_CONFIG_DIR" ==
"test/config-next"`.
- `Balancer.Build()` in "noncebalancer" forces a rebuild until non-zero
backends are available. This matches the
[balancer/roundrobin](https://github.com/grpc/grpc-go/blob/d524b409462c601ef3f05a7e1fba19755a337c77/balancer/roundrobin/roundrobin.go#L49-L53)
implementation.
- Nonces with no matching backend increment "jose_errors" with label
`"type": "JWSInvalidNonce"` and "nonce_no_backend_found".
- Nonces of incorrect length are now rejected at the WFE and increment
"jose_errors" with label `"type": "JWSMalformedNonce"` instead of
`"type": "JWSInvalidNonce"`.
- Nonces not encoded as base64url are now rejected at the WFE and
increment "jose_errors" with label `"type": "JWSMalformedNonce"` instead
of `"type": "JWSInvalidNonce"`.

Fixes #6969
Part of #6974

grpc/noncebalancer/noncebalancer.go
@@ -4,8 +4,11 @@ import (
 	"errors"
 
 	"github.com/letsencrypt/boulder/nonce"
+
 	"google.golang.org/grpc/balancer"
 	"google.golang.org/grpc/balancer/base"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 const (
@@ -20,6 +23,15 @@ const (
 	SRVResolverScheme = "nonce-srv"
 )
 
+// ErrNoBackendsMatchPrefix indicates that no backends were found which match
+// the nonce prefix provided in the RPC context. This can happen when the
+// provided nonce is stale, valid but the backend has since been removed from
+// the balancer, or valid but the backend has not yet been added to the
+// balancer.
+//
+// In any case, when the WFE receives this error it will return a badNonce error
+// to the ACME client.
+var ErrNoBackendsMatchPrefix = status.New(codes.Unavailable, "no backends match the nonce prefix")
 var errMissingPrefixCtxKey = errors.New("nonce.PrefixCtxKey value required in RPC context")
 var errMissingHMACKeyCtxKey = errors.New("nonce.HMACKeyCtxKey value required in RPC context")
 var errInvalidPrefixCtxKeyType = errors.New("nonce.PrefixCtxKey value in RPC context must be a string")
@@ -35,9 +47,12 @@ var _ base.PickerBuilder = (*Balancer)(nil)
 
 // Build implements the base.PickerBuilder interface. It is called by the gRPC
 // runtime when the balancer is first initialized and when the set of backend
-// (SubConn) addresses changes. It is responsible for initializing the Picker's
-// backends map and returning a balancer.Picker.
+// (SubConn) addresses changes.
 func (b *Balancer) Build(buildInfo base.PickerBuildInfo) balancer.Picker {
+	if len(buildInfo.ReadySCs) == 0 {
+		// The Picker must be rebuilt if there are no backends available.
+		return base.NewErrPicker(balancer.ErrNoSubConnAvailable)
+	}
 	return &Picker{
 		backends: buildInfo.ReadySCs,
 	}
@@ -58,7 +73,8 @@ var _ balancer.Picker = (*Picker)(nil)
 // (SubConn) based on the context of each RPC message.
 func (p *Picker) Pick(info balancer.PickInfo) (balancer.PickResult, error) {
 	if len(p.backends) == 0 {
-		// The Picker must be rebuilt if there are no backends available.
+		// This should never happen, the Picker should only be built when there
+		// are backends available.
 		return balancer.PickResult{}, balancer.ErrNoSubConnAvailable
 	}
 
@@ -75,8 +91,7 @@ func (p *Picker) Pick(info balancer.PickInfo) (balancer.PickResult, error) {
 	}
 
 	if p.prefixToBackend == nil {
-		// Iterate over the backends and build a map of the derived prefix for
-		// each backend SubConn.
+		// First call to Pick with a new Picker.
 		prefixToBackend := make(map[string]balancer.SubConn)
 		for sc, scInfo := range p.backends {
 			scPrefix := nonce.DerivePrefix(scInfo.Address.Addr, hmacKey)
@@ -100,7 +115,7 @@ func (p *Picker) Pick(info balancer.PickInfo) (balancer.PickResult, error) {
 	sc, ok := p.prefixToBackend[destPrefix]
 	if !ok {
 		// No backend SubConn was found for the destination prefix.
-		return balancer.PickResult{}, balancer.ErrNoSubConnAvailable
+		return balancer.PickResult{}, ErrNoBackendsMatchPrefix.Err()
 	}
 	return balancer.PickResult{SubConn: sc}, nil
 }

nonce/nonce.go
@@ -38,13 +38,16 @@ import (
 const (
 	// PrefixLen is the character length of a nonce prefix.
 	PrefixLen = 8
+
 	// DeprecatedPrefixLen is the character length of a nonce prefix.
 	//
 	// DEPRECATED: Use PrefixLen instead.
 	// TODO(#6610): Remove once we've moved to derivable prefixes by default.
 	DeprecatedPrefixLen = 4
-	defaultMaxUsed      = 65536
-	nonceLen            = 32
+
+	// NonceLen is the character length of a nonce, excluding the prefix.
+	NonceLen       = 32
+	defaultMaxUsed = 65536
 )
 
 var errInvalidNonceLength = errors.New("invalid nonce length")
@@ -203,7 +206,7 @@ func (ns *NonceService) encrypt(counter int64) (string, error) {
 	copy(pt[pad:], ctr.Bytes())
 
 	// Encrypt
-	ret := make([]byte, nonceLen)
+	ret := make([]byte, NonceLen)
 	ct := ns.gcm.Seal(nil, nonce, pt, nil)
 	copy(ret, nonce[4:])
 	copy(ret[8:], ct)
@@ -228,7 +231,7 @@ func (ns *NonceService) decrypt(nonce string) (int64, error) {
 	if err != nil {
 		return 0, err
 	}
-	if len(decoded) != nonceLen {
+	if len(decoded) != NonceLen {
 		return 0, errInvalidNonceLength
 	}
 

wfe2/stats.go
@@ -17,6 +17,9 @@ type wfe2Stats struct {
 	// improperECFieldLengths counts the number of ACME account EC JWKs we see
 	// with improper X and Y lengths for their curve
 	improperECFieldLengths prometheus.Counter
+	// nonceNoMatchingBackendCount counts the number of times we've received a nonce
+	// with a prefix that doesn't match a known backend.
+	nonceNoMatchingBackendCount prometheus.Counter
 }
 
 func initStats(stats prometheus.Registerer) wfe2Stats {
@@ -53,10 +56,19 @@ func initStats(stats prometheus.Registerer) wfe2Stats {
 	)
 	stats.MustRegister(improperECFieldLengths)
 
+	nonceNoBackendCount := prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "nonce_no_backend_found",
+			Help: "Number of times we've received a nonce with a prefix that doesn't match a known backend",
+		},
+	)
+	stats.MustRegister(nonceNoBackendCount)
+
 	return wfe2Stats{
-		httpErrorCount:         httpErrorCount,
-		joseErrorCount:         joseErrorCount,
-		csrSignatureAlgs:       csrSignatureAlgs,
-		improperECFieldLengths: improperECFieldLengths,
+		httpErrorCount:              httpErrorCount,
+		joseErrorCount:              joseErrorCount,
+		csrSignatureAlgs:            csrSignatureAlgs,
+		improperECFieldLengths:      improperECFieldLengths,
+		nonceNoMatchingBackendCount: nonceNoBackendCount,
 	}
 }

wfe2/verify.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"crypto/ecdsa"
 	"crypto/rsa"
+	"encoding/base64"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -14,12 +15,14 @@ import (
 	"strings"
 
 	"github.com/prometheus/client_golang/prometheus"
+	"google.golang.org/grpc/status"
 	"gopkg.in/go-jose/go-jose.v2"
 
 	"github.com/letsencrypt/boulder/core"
 	berrors "github.com/letsencrypt/boulder/errors"
 	"github.com/letsencrypt/boulder/goodkey"
 	"github.com/letsencrypt/boulder/grpc"
+	nb "github.com/letsencrypt/boulder/grpc/noncebalancer"
 	"github.com/letsencrypt/boulder/nonce"
 	noncepb "github.com/letsencrypt/boulder/nonce/proto"
 	"github.com/letsencrypt/boulder/probs"
@@ -183,6 +186,23 @@ func (wfe *WebFrontEndImpl) validPOSTRequest(request *http.Request) *probs.Probl
 	return nil
 }
 
+// nonceWellFormed checks a JWS' Nonce header to ensure it is well-formed,
+// otherwise a bad nonce problem is returned. This avoids unnecessary RPCs to
+// the nonce redemption service.
+func nonceWellFormed(nonceHeader string, prefixLen int) *probs.ProblemDetails {
+	errBadNonce := probs.BadNonce(fmt.Sprintf("JWS has an invalid anti-replay nonce: %q", nonceHeader))
+	body, err := base64.RawURLEncoding.DecodeString(nonceHeader[prefixLen:])
+	if err != nil {
+		// Nonce was not valid base64url.
+		return errBadNonce
+	}
+	if len(body) != nonce.NonceLen {
+		// Nonce was an unexpected length.
+		return errBadNonce
+	}
+	return nil
+}
+
 // validNonce checks a JWS' Nonce header to ensure it is one that the
 // nonceService knows about, otherwise a bad nonce problem is returned.
 // NOTE: this function assumes the JWS has already been verified with the
@@ -196,17 +216,43 @@ func (wfe *WebFrontEndImpl) validNonce(ctx context.Context, header jose.Header) 
 	var err error
 	if wfe.noncePrefixMap == nil {
 		// Dispatch nonce redemption RPCs dynamically.
+		prob := nonceWellFormed(header.Nonce, nonce.PrefixLen)
+		if prob != nil {
+			wfe.stats.joseErrorCount.With(prometheus.Labels{"type": "JWSMalformedNonce"}).Inc()
+			return prob
+		}
+
+		// Populate the context with the nonce prefix and HMAC key. These are
+		// used by a custom gRPC balancer, known as "noncebalancer", to route
+		// redemption RPCs to the backend that originally issued the nonce.
 		ctx = context.WithValue(ctx, nonce.PrefixCtxKey{}, header.Nonce[:nonce.PrefixLen])
 		ctx = context.WithValue(ctx, nonce.HMACKeyCtxKey{}, wfe.rncKey)
+
 		resp, err := wfe.rnc.Redeem(ctx, &noncepb.NonceMessage{Nonce: header.Nonce})
 		if err != nil {
-			return web.ProblemDetailsForError(err, "failed to redeem nonce")
+			rpcStatus, ok := status.FromError(err)
+			if !ok || rpcStatus != nb.ErrNoBackendsMatchPrefix {
+				return web.ProblemDetailsForError(err, "failed to redeem nonce")
+			}
+
+			// ErrNoBackendsMatchPrefix suggests that the nonce backend, which
+			// issued this nonce, is presently unreachable or unrecognized by
+			// this WFE. As this is a transient failure, the client should retry
+			// their request with a fresh nonce.
+			resp = &noncepb.ValidMessage{Valid: false}
+			wfe.stats.nonceNoMatchingBackendCount.Inc()
 		}
 		valid = resp.Valid
 	} else {
 		// Dispatch nonce redpemption RPCs using a static mapping.
 		//
 		// TODO(#6610) Remove code below and the `npm` mapping.
+		prob := nonceWellFormed(header.Nonce, nonce.DeprecatedPrefixLen)
+		if prob != nil {
+			wfe.stats.joseErrorCount.With(prometheus.Labels{"type": "JWSMalformedNonce"}).Inc()
+			return prob
+		}
+
 		valid, err = nonce.RemoteRedeem(ctx, wfe.noncePrefixMap, header.Nonce)
 		if err != nil {
 			return web.ProblemDetailsForError(err, "failed to redeem nonce")

grpc/noncebalancer/noncebalancer_test.go
@@ -80,7 +80,7 @@ func TestPickerNoMatchingSubConnAvailable(t *testing.T) {
 	info := balancer.PickInfo{Ctx: testCtx}
 
 	gotPick, err := p.Pick(info)
-	test.AssertErrorIs(t, err, balancer.ErrNoSubConnAvailable)
+	test.AssertErrorIs(t, err, ErrNoBackendsMatchPrefix.Err())
 	test.AssertNil(t, gotPick.SubConn, "subConn should be nil")
 }
 

test/integration/nonce_test.go
@@ -1,0 +1,68 @@
+//go:build integration
+
+package integration
+
+import (
+	"context"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/jmhodges/clock"
+
+	"github.com/letsencrypt/boulder/cmd"
+	bgrpc "github.com/letsencrypt/boulder/grpc"
+	nb "github.com/letsencrypt/boulder/grpc/noncebalancer"
+	"github.com/letsencrypt/boulder/metrics"
+	"github.com/letsencrypt/boulder/nonce"
+	noncepb "github.com/letsencrypt/boulder/nonce/proto"
+	"github.com/letsencrypt/boulder/test"
+	"google.golang.org/grpc/status"
+)
+
+type Config struct {
+	NotWFE struct {
+		TLS                cmd.TLSConfig
+		GetNonceService    *cmd.GRPCClientConfig
+		RedeemNonceService *cmd.GRPCClientConfig
+		NoncePrefixKey     cmd.PasswordConfig
+	}
+}
+
+func TestNonceBalancer_NoBackendMatchingPrefix(t *testing.T) {
+	t.Parallel()
+
+	if !strings.Contains(os.Getenv("BOULDER_CONFIG_DIR"), "test/config-next") {
+		t.Skip("Derived nonce prefixes are only configured in config-next")
+	}
+
+	// We're going to use a minimal nonce service client called "notwfe" which
+	// masquerades as a wfe for the purpose of redeeming nonces.
+
+	// Load the test config.
+	var c Config
+	err := cmd.ReadConfigFile("test/integration/testdata/nonce-client.json", &c)
+	test.AssertNotError(t, err, "Could not read config file")
+
+	tlsConfig, err := c.NotWFE.TLS.Load(metrics.NoopRegisterer)
+	test.AssertNotError(t, err, "Could not load TLS config")
+
+	rncKey, err := c.NotWFE.NoncePrefixKey.Pass()
+	test.AssertNotError(t, err, "Failed to load noncePrefixKey")
+
+	clk := clock.New()
+
+	redeemNonceConn, err := bgrpc.ClientSetup(c.NotWFE.RedeemNonceService, tlsConfig, metrics.NoopRegisterer, clk)
+	test.AssertNotError(t, err, "Failed to load credentials and create gRPC connection to redeem nonce service")
+	rnc := nonce.NewRedeemer(redeemNonceConn)
+
+	// Attempt to redeem a nonce with a prefix that doesn't match any backends.
+	ctx := context.WithValue(context.Background(), nonce.PrefixCtxKey{}, "12345678")
+	ctx = context.WithValue(ctx, nonce.HMACKeyCtxKey{}, rncKey)
+	_, err = rnc.Redeem(ctx, &noncepb.NonceMessage{Nonce: "0123456789"})
+
+	// We expect to get a specific gRPC status error with code NotFound.
+	gotRPCStatus, ok := status.FromError(err)
+	test.Assert(t, ok, "Failed to convert error to status")
+	test.AssertEquals(t, gotRPCStatus, nb.ErrNoBackendsMatchPrefix)
+}

wfe2/verify_test.go
@@ -9,6 +9,7 @@ import (
 	"crypto/rsa"
 	"fmt"
 	"net/http"
+	"os"
 	"strings"
 	"testing"
 
@@ -16,7 +17,9 @@ import (
 	corepb "github.com/letsencrypt/boulder/core/proto"
 	"github.com/letsencrypt/boulder/goodkey"
 	bgrpc "github.com/letsencrypt/boulder/grpc"
+	"github.com/letsencrypt/boulder/grpc/noncebalancer"
 	"github.com/letsencrypt/boulder/mocks"
+	noncepb "github.com/letsencrypt/boulder/nonce/proto"
 	"github.com/letsencrypt/boulder/probs"
 	sapb "github.com/letsencrypt/boulder/sa/proto"
 	"github.com/letsencrypt/boulder/test"
@@ -215,6 +218,39 @@ func (rs requestSigner) invalidNonce() *jose.JSONWebSignature {
 
 	opts := &jose.SignerOptions{
 		NonceSource: badNonceProvider{},
+		ExtraHeaders: map[jose.HeaderKey]interface{}{
+			"url": "https://example.com/acme/foo",
+		},
+	}
+
+	signer, err := jose.NewSigner(signerKey, opts)
+	test.AssertNotError(rs.t, err, "Failed to make signer")
+	jws, err := signer.Sign([]byte(""))
+	test.AssertNotError(rs.t, err, "Failed to sign req")
+
+	body := jws.FullSerialize()
+	parsedJWS, err := jose.ParseSigned(body)
+	test.AssertNotError(rs.t, err, "Failed to parse generated JWS")
+
+	return parsedJWS
+}
+
+// malformedNonce returns an otherwise well-signed request with a malformed
+// nonce.
+func (rs requestSigner) malformedNonce() *jose.JSONWebSignature {
+	privateKey := loadKey(rs.t, []byte(test1KeyPrivatePEM))
+	jwk := &jose.JSONWebKey{
+		Key:       privateKey,
+		Algorithm: keyAlgForKey(rs.t, privateKey),
+		KeyID:     "http://localhost/acme/acct/1",
+	}
+	signerKey := jose.SigningKey{
+		Key:       jwk,
+		Algorithm: jose.RS256,
+	}
+
+	opts := &jose.SignerOptions{
+		NonceSource: badNonceProvider{malformed: true},
 		ExtraHeaders: map[jose.HeaderKey]interface{}{
 			"url": "https://example.com/acme/foo",
 		},
@@ -630,10 +666,18 @@ func TestEnforceJWSAuthType(t *testing.T) {
 }
 
 type badNonceProvider struct {
+	malformed bool
 }
 
-func (badNonceProvider) Nonce() (string, error) {
-	return "im-a-nonce", nil
+func (b badNonceProvider) Nonce() (string, error) {
+	if b.malformed {
+		return "im-a-nonce", nil
+	}
+	if os.Getenv("BOULDER_CONFIG_DIR") != "test/config-next" {
+		// TODO(#6610): Remove this.
+		return "mlol3ov77I5Ui-cdaY_k8IcjK58FvbG0y_BCRrx5rGQ8rjA", nil
+	}
+	return "mlolmlol3ov77I5Ui-cdaY_k8IcjK58FvbG0y_BCRrx5rGQ8rjA", nil
 }
 
 func TestValidNonce(t *testing.T) {
@@ -646,6 +690,10 @@ func TestValidNonce(t *testing.T) {
 		JWS            *jose.JSONWebSignature
 		ExpectedResult *probs.ProblemDetails
 		ErrorStatType  string
+		// TODO(#6610): Remove this.
+		SkipConfigNext bool
+		// TODO(#6610): Remove this.
+		SkipConfig bool
 	}{
 		{
 			Name: "No nonce in JWS",
@@ -658,14 +706,36 @@ func TestValidNonce(t *testing.T) {
 			ErrorStatType: "JWSMissingNonce",
 		},
 		{
-			Name: "Invalid nonce in JWS",
-			JWS:  signer.invalidNonce(),
+			Name: "Malformed nonce in JWS",
+			JWS:  signer.malformedNonce(),
 			ExpectedResult: &probs.ProblemDetails{
 				Type:       probs.BadNonceProblem,
 				Detail:     "JWS has an invalid anti-replay nonce: \"im-a-nonce\"",
 				HTTPStatus: http.StatusBadRequest,
 			},
+			ErrorStatType: "JWSMalformedNonce",
+		},
+		{
+			Name: "Invalid nonce in JWS (test/config)",
+			JWS:  signer.invalidNonce(),
+			ExpectedResult: &probs.ProblemDetails{
+				Type:       probs.BadNonceProblem,
+				Detail:     "JWS has an invalid anti-replay nonce: \"mlol3ov77I5Ui-cdaY_k8IcjK58FvbG0y_BCRrx5rGQ8rjA\"",
+				HTTPStatus: http.StatusBadRequest,
+			},
+			ErrorStatType:  "JWSInvalidNonce",
+			SkipConfigNext: true,
+		},
+		{
+			Name: "Invalid nonce in JWS (test/config-next)",
+			JWS:  signer.invalidNonce(),
+			ExpectedResult: &probs.ProblemDetails{
+				Type:       probs.BadNonceProblem,
+				Detail:     "JWS has an invalid anti-replay nonce: \"mlolmlol3ov77I5Ui-cdaY_k8IcjK58FvbG0y_BCRrx5rGQ8rjA\"",
+				HTTPStatus: http.StatusBadRequest,
+			},
 			ErrorStatType: "JWSInvalidNonce",
+			SkipConfig:    true,
 		},
 		{
 			Name:           "Valid nonce in JWS",
@@ -676,6 +746,14 @@ func TestValidNonce(t *testing.T) {
 
 	for _, tc := range testCases {
 		t.Run(tc.Name, func(t *testing.T) {
+			// TODO(#6610): Remove this.
+			if os.Getenv("BOULDER_CONFIG_DIR") == "test/config-next" {
+				if tc.SkipConfigNext {
+					t.Skip("Skipping test in config-next")
+				}
+			} else if tc.SkipConfig {
+				t.Skip("Skipping test in config")
+			}
 			wfe.stats.joseErrorCount.Reset()
 			prob := wfe.validNonce(context.Background(), tc.JWS.Signatures[0].Header)
 			if tc.ExpectedResult == nil && prob != nil {
@@ -689,6 +767,33 @@ func TestValidNonce(t *testing.T) {
 			}
 		})
 	}
+}
+
+// noBackendsNonceRedeemer is a nonce redeemer that always returns an error
+// indicating that the prefix matches no known nonce provider.
+type noBackendsNonceRedeemer struct{}
+
+func (n noBackendsNonceRedeemer) Redeem(ctx context.Context, _ *noncepb.NonceMessage, opts ...grpc.CallOption) (*noncepb.ValidMessage, error) {
+	return nil, noncebalancer.ErrNoBackendsMatchPrefix.Err()
+}
+
+func TestValidNonce_NoMatchingBackendFound(t *testing.T) {
+	// TODO(#6610): Remove this.
+	if os.Getenv("BOULDER_CONFIG_DIR") != "test/config-next" {
+		t.Skip("Skipping test in config")
+	}
+	wfe, _, signer := setupWFE(t)
+	goodJWS, _, _ := signer.embeddedJWK(nil, "", "")
+	wfe.rnc = noBackendsNonceRedeemer{}
+
+	// A valid JWS with a nonce whose prefix matches no known nonce provider should
+	// result in a BadNonceProblem.
+	prob := wfe.validNonce(context.Background(), goodJWS.Signatures[0].Header)
+	test.Assert(t, prob != nil, "Expected error for valid nonce with no backend")
+	test.AssertEquals(t, prob.Type, probs.BadNonceProblem)
+	test.AssertEquals(t, prob.HTTPStatus, http.StatusBadRequest)
+	test.AssertContains(t, prob.Detail, "JWS has an invalid anti-replay nonce")
+	test.AssertMetricWithLabelsEquals(t, wfe.stats.nonceNoMatchingBackendCount, prometheus.Labels{}, 1)
 }
 
 func (rs requestSigner) signExtraHeaders(
@@ -1231,6 +1336,10 @@ func TestValidJWSForKey(t *testing.T) {
 		Body            string
 		ExpectedProblem *probs.ProblemDetails
 		ErrorStatType   string
+		// TODO(#6610): Remove this.
+		SkipConfig bool
+		// TODO(#6610): Remove this.
+		SkipConfigNext bool
 	}{
 		{
 			Name: "JWS with an invalid algorithm",
@@ -1244,15 +1353,28 @@ func TestValidJWSForKey(t *testing.T) {
 			ErrorStatType: "JWSAlgorithmCheckFailed",
 		},
 		{
-			Name: "JWS with an invalid nonce",
+			Name: "JWS with an invalid nonce (test/config)",
 			JWS:  bJSONWebSignature{signer.invalidNonce()},
 			JWK:  goodJWK,
 			ExpectedProblem: &probs.ProblemDetails{
 				Type:       probs.BadNonceProblem,
-				Detail:     "JWS has an invalid anti-replay nonce: \"im-a-nonce\"",
+				Detail:     "JWS has an invalid anti-replay nonce: \"mlol3ov77I5Ui-cdaY_k8IcjK58FvbG0y_BCRrx5rGQ8rjA\"",
+				HTTPStatus: http.StatusBadRequest,
+			},
+			ErrorStatType:  "JWSInvalidNonce",
+			SkipConfigNext: true,
+		},
+		{
+			Name: "JWS with an invalid nonce (test/config-next)",
+			JWS:  bJSONWebSignature{signer.invalidNonce()},
+			JWK:  goodJWK,
+			ExpectedProblem: &probs.ProblemDetails{
+				Type:       probs.BadNonceProblem,
+				Detail:     "JWS has an invalid anti-replay nonce: \"mlolmlol3ov77I5Ui-cdaY_k8IcjK58FvbG0y_BCRrx5rGQ8rjA\"",
 				HTTPStatus: http.StatusBadRequest,
 			},
 			ErrorStatType: "JWSInvalidNonce",
+			SkipConfig:    true,
 		},
 		{
 			Name: "JWS with broken signature",
@@ -1295,7 +1417,16 @@ func TestValidJWSForKey(t *testing.T) {
 	}
 
 	for _, tc := range testCases {
+		// TODO(#6610): Remove this.
 		t.Run(tc.Name, func(t *testing.T) {
+			// TODO(#6610): Remove this.
+			if os.Getenv("BOULDER_CONFIG_DIR") == "test/config-next" {
+				if tc.SkipConfigNext {
+					t.Skip("Skipping test in config-next")
+				}
+			} else if tc.SkipConfig {
+				t.Skip("Skipping test in config")
+			}
 			wfe.stats.joseErrorCount.Reset()
 			request := makePostRequestWithPath("test", tc.Body)
 			outPayload, prob := wfe.validJWSForKey(context.Background(), &tc.JWS, tc.JWK, request)

wfe2/wfe_test.go
@@ -339,11 +339,31 @@ func setupWFE(t *testing.T) (WebFrontEndImpl, clock.FakeClock, requestSigner) {
 	}
 
 	mockSA := mocks.NewStorageAuthorityReadOnly(fc)
-	noncePrefix := "mlem"
-	nonceService, err := nonce.NewNonceService(metrics.NoopRegisterer, 100, noncePrefix)
-	test.AssertNotError(t, err, "making nonceService")
-	nonceGRPCService := &inmemnonce.Service{
-		NonceService: nonceService,
+
+	var gnc nonce.Getter
+	var noncePrefixMap map[string]nonce.Redeemer
+	var rnc nonce.Redeemer
+	var rncKey string
+	var inmemNonceService *inmemnonce.Service
+	if strings.Contains(os.Getenv("BOULDER_CONFIG_DIR"), "test/config-next") {
+		// Use derived nonces.
+		noncePrefix := nonce.DerivePrefix("192.168.1.1:8080", "b8c758dd85e113ea340ce0b3a99f389d40a308548af94d1730a7692c1874f1f")
+		nonceService, err := nonce.NewNonceService(metrics.NoopRegisterer, 100, noncePrefix)
+		test.AssertNotError(t, err, "making nonceService")
+
+		inmemNonceService = &inmemnonce.Service{NonceService: nonceService}
+		gnc = inmemNonceService
+		rnc = inmemNonceService
+	} else {
+		// TODO(#6610): Remove this once we've moved to derived to prefixes.
+		noncePrefix := "mlem"
+		nonceService, err := nonce.NewNonceService(metrics.NoopRegisterer, 100, noncePrefix)
+		test.AssertNotError(t, err, "making nonceService")
+
+		inmemNonceService = &inmemnonce.Service{NonceService: nonceService}
+		gnc = inmemNonceService
+		noncePrefixMap = map[string]nonce.Redeemer{noncePrefix: inmemNonceService}
+		rnc = inmemNonceService
 	}
 
 	wfe, err := NewWebFrontEndImpl(
@@ -359,16 +379,16 @@ func setupWFE(t *testing.T) (WebFrontEndImpl, clock.FakeClock, requestSigner) {
 		7*24*time.Hour,
 		&MockRegistrationAuthority{},
 		mockSA,
-		nonceGRPCService,
-		map[string]nonce.Redeemer{noncePrefix: nonceGRPCService},
-		nonceGRPCService,
-		"",
+		gnc,
+		noncePrefixMap,
+		rnc,
+		rncKey,
 		mockSA)
 	test.AssertNotError(t, err, "Unable to create WFE")
 
 	wfe.SubscriberAgreementURL = agreementURL
 
-	return wfe, fc, requestSigner{t, nonceGRPCService.AsSource()}
+	return wfe, fc, requestSigner{t, inmemNonceService.AsSource()}
 }
 
 // makePostRequestWithPath creates an http.Request for localhost with method